82b9f550b83d1846e64128bd4d007e4bf0de8073
Hari Vasantham <narahariv94@gmail.com>
2025-04-06 16:56:18 +0530

    login functionality added

---
+++ src/components/Body.js
@@ -0,0 +1,23 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Browse from './Browse';
+import Login from './Login';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Login />,
+  },
+  {
+    path: '/browse',
+    element: <Browse />,
+  },
+]);
+
+const Body = () => {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+};
+export default Body;
